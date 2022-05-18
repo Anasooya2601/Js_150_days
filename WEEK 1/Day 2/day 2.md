2f1ce72bd5bda263921662be47ec0ba4d56c74d5
Anasooya <anasooyadutta@gmail.com>
2022-05-18 12:44:04 +0530

    added hoisting examples

--- WEEK 1/Day 2/day 2.md
+++ WEEK 1/Day 2/day 2.md
@@ -6,6 +6,15 @@ Q Define hoisting
 answer
 </summary>
 <p>Hoisting is when the <b>JavaScript interpreter</b> moves all variable and function declarations to the top of the current scope.</p>
+<h4>
+`JS interpreter` first scans the whole code.
+<li>
+variable declaration and function declaration
+</li>
+<li>
+executes the code
+</li>
+</h4>
 </details>
 Exercise
 
@@ -22,6 +31,31 @@ Q2.
 console.log(bar);
 var bar = 'bar';
 console.log(bar);
+```
+Q3.
+
+```JAVASCRIPT
+foo();
+function foo() {
+    console.log(bam);
+    var bam = 'bam';
+}
+
+console.log(bam);
+
+```
+
+Q4.
+
+```JAVASCRIPT
+
+function foo() {
+    var bam;
+    console.log(bam);
+    bam = 'bam';
+}
+foo();
+console.log(bam);
 
 ```
 
--- WEEK 1/Day 2/day2.js
+++ WEEK 1/Day 2/day2.js
@@ -6,18 +6,77 @@
 // console.log()
 
 //hoisting
+
 ///1.)VARIABLE
 // console.log(bar);
 // var bar = 'bar';
 // console.log(bar);
+
+// -------------
 
 //q2) VAR7ABLE
 // let a=undefined;
 // console.log("1",a);
 // a=34
 
+// ------------
+
 //q3) DECLARE VARIABLE
-var bar;
-console.log(bar); // undefined
-bar = 'bar';
-console.log(bar); // 'bar'
+
+// var bar;
+// console.log(bar); // undefined
+// bar = 'bar';
+// console.log(bar); // 'bar'
+//Function Declarations
+// foo();
+// function foo() {
+//     console.log(bam);
+//     var bam = 'bam';
+// }
+
+// console.log(bam);
+
+// ------------
+
+// var bam
+// function foo() {
+//     var bam;
+//     console.log(bam);
+//     bam = 'bam';
+// }
+// foo();
+// console.log(bam);
+
+
+//--------x------
+
+// foo()
+// const foo=()=>{
+//     var bam;
+//     console.log(bam)
+// }
+
+//------------------
+// var bar
+// function foo(){
+//     var bar="bar"
+//     console.log(bar)
+//     }
+//     foo()
+// console.log(bar)
+
+//---------
+// console,log(ban)
+// var ban;
+// console.log(ba
+
+//-----
+
+function foo()
+{
+    console.log(name)
+    var name="ana"
+  
+    
+}
+foo()
